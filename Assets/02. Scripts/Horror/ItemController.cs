2fbea2dd8c5182fd64e43c4d8f9893c4b2d69fe7
woojeong <77192050+wooj22@users.noreply.github.com>
2024-09-19 00:55:25 +0900

    🔨 Feat | 아이템 알고리즘

--- Assets/02. Scripts/Horror/ItemController.cs
+++ Assets/02. Scripts/Horror/ItemController.cs
@@ -3,15 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class GhostController : MonoBehaviour
+public class ItemController : MonoBehaviour
 {
-    [SerializeField] Animator goshAnimator;
-
-    private void Start()
-    {
-        goshAnimator = GetComponent<Animator>();
-    }
-
     /// 플레이어와 충돌
     private void OnTriggerEnter(Collider other)
     {
@@ -23,8 +16,7 @@ public class GhostController : MonoBehaviour
 
     IEnumerator FoundPlayer()
     {
-        Debug.Log("귀신 발견");
-        goshAnimator.Play("Attack", 0, 0);
+        Debug.Log("아이템 습득");
         yield return new WaitForSeconds(1f);
         Destroy(this.gameObject);
     }
