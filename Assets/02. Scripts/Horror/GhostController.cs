f19c610917659af29ce634f8cbc4cf3cd1f4fc4e
woojeong <77192050+wooj22@users.noreply.github.com>
2024-09-19 00:25:38 +0900

    🔨 Feat | 귀신 알고리즘
    플레이어 발견하면 겁주고 사라짐

---
+++ Assets/02. Scripts/Horror/GhostController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GhostController : MonoBehaviour
+{
+    [SerializeField] Animator goshAnimator;
+
+    private void Start()
+    {
+        goshAnimator = GetComponent<Animator>();
+    }
+
+    /// 플레이어와 충돌
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            StartCoroutine(FoundPlayer());
+        }
+    }
+
+    IEnumerator FoundPlayer()
+    {
+        Debug.Log("플레이어 발견!");
+        goshAnimator.Play("Attack", 0, 0);
+        yield return new WaitForSeconds(1f);
+        Destroy(this.gameObject);
+    }
+}
