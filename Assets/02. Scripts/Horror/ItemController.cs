ec91c3997727ef9eb9a5fb99f89f556921605ed9
woojeong <77192050+wooj22@users.noreply.github.com>
2024-09-19 04:51:38 +0900

    🔨 Feat | 귀신 URP용 머테리얼 적용 지향이 최고

--- Assets/02. Scripts/Horror/ItemController.cs
+++ Assets/02. Scripts/Horror/ItemController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
+    public HorrorGameManager _gameManager;
+    //eatItemCount
+
+    private void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<HorrorGameManager>();
+    }
     /// 플레이어와 충돌
     private void OnTriggerEnter(Collider other)
     {
