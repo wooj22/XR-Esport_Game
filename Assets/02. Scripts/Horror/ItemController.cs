cb16950cf496f14f85052875b230030945805996
woojeong <77192050+wooj22@users.noreply.github.com>
2024-09-21 03:10:08 +0900

    🔨 Feat | UIManager, GameManager 완

--- Assets/02. Scripts/Horror/ItemController.cs
+++ Assets/02. Scripts/Horror/ItemController.cs
@@ -6,26 +6,25 @@ using UnityEngine;
 public class ItemController : MonoBehaviour
 {
     public HorrorGameManager _gameManager;
-    //eatItemCount
 
     private void Start()
     {
         _gameManager = GameObject.Find("GameManager").GetComponent<HorrorGameManager>();
     }
+
     /// 플레이어와 충돌
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            StartCoroutine(FoundPlayer());
+            _gameManager.OnPlayerEatItem();
+            StartCoroutine(Wait1F());
+            Destroy(this.gameObject);
         }
     }
 
-    IEnumerator FoundPlayer()
+    IEnumerator Wait1F()
     {
-        Debug.Log("아이템 습득");
-        _gameManager.eatItemCount++;
         yield return new WaitForSeconds(1f);
-        Destroy(this.gameObject);
     }
 }
