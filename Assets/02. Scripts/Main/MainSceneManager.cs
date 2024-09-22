80a3d8f24ac268e87d9779129d3ad89f94d98c5b
woojeong <77192050+wooj22@users.noreply.github.com>
2024-09-22 21:09:31 +0900

    🔨 Feat | 메인광장 MainSceneManager
     - 회전목마랑 자이로드롭은 콜라이더 켜기만하면 됨. 지금은 맵이 없어서 오류나서 꺼둠

--- Assets/02. Scripts/Main/MainSceneManager.cs
+++ Assets/02. Scripts/Main/MainSceneManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] MainSoundManager _mainSoundManager;
+
+    /// æ¿ ¿Ãµø
+    public void OnLoadSceneByName(string sceneName)
     {
-        
+        _mainSoundManager.PlaySFX("SFX_Main_openMap");
+        StartCoroutine(SoundWaiting(sceneName));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SoundWaiting(string sceneName)
     {
-        
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(sceneName);
     }
 }
