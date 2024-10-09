4418a32b65e99e8d838eab5d696763537b2cfe71
woojeong <77192050+wooj22@users.noreply.github.com>
2024-10-10 02:44:54 +0900

    🔨 Feat | 겹 레이저 프리팹 3개

--- Assets/02. Scripts/Circus/CircusGameManager.cs
+++ Assets/02. Scripts/Circus/CircusGameManager.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class CircusGameManager : MonoBehaviour
 {
     [Header("Levels")]
+    [SerializeField] float currentLaserCycle;      // 현재의 레이저 발생 주기
+    [SerializeField] float currentLaserSpeed;      // 현재의 레이저 속도
+
+    [Header("MapData")]
     [SerializeField] float gamePlayTime;           // 전체 게임 진행 시간 120초
-    [SerializeField] float currentAttackCycle;     // 현재의 동물 공격 주기
-    [SerializeField] float currentBallSpeed;       // 현재의 공 속도
+    [SerializeField] int maxLaserCount;            // 전체 레이저 개수 29개
+    [SerializeField] List<GameObject> laserPrefabList;
 
     [Header("Managers")]
     [SerializeField] CircusUIManager _circusUIManager;
@@ -19,5 +23,6 @@ public class CircusGameManager : MonoBehaviour
         _circusSoundManager.PlayBGM();
         _circusUIManager.StartCountDown(5);
         _circusUIManager.StartTimer(gamePlayTime);
+        _circusUIManager.GaugeSetting(5);
     }
 }
