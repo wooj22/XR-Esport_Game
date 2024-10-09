dea515830e4a3e8c876b6b8987be8818a1dd266a
woojeong <77192050+wooj22@users.noreply.github.com>
2024-10-10 00:37:16 +0900

    🔨 Feat | 레이저 VFX 프리팹 제작

--- Assets/02. Scripts/Circus/CircusGameManager.cs
+++ Assets/02. Scripts/Circus/CircusGameManager.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class CircusGameManager : MonoBehaviour
 {
-    
+    [Header("Levels")]
+    [SerializeField] float gamePlayTime;           // 전체 게임 진행 시간 120초
+    [SerializeField] float currentAttackCycle;     // 현재의 동물 공격 주기
+    [SerializeField] float currentBallSpeed;       // 현재의 공 속도
+
+    [Header("Managers")]
+    [SerializeField] CircusUIManager _circusUIManager;
+    [SerializeField] CircusSoundManager _circusSoundManager;
+    [SerializeField] CircusSceneManager _circusSceneManager;
 }
--- Assets/02. Scripts/Circus/CircusUIManager.cs
+++ Assets/02. Scripts/Circus/CircusUIManager.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CircusUIManager : MonoBehaviour
 {
-    
+    [SerializeField] Text timerLable;
+    [SerializeField] Slider gaugeBar;
+    [SerializeField] Text adviceLable;
+    [SerializeField] Image adviceBackImage;
+
+    private int startCount;
+
+    public void StartCountDown()
+    {
+        startCount = 5;
+        StartCoroutine(StartCountDown2());
+    }
+
+    IEnumerator StartCountDown2()
+    {
+        yield return new WaitForSeconds(1f);
+        adviceLable.text = startCount.ToString();
+    }
 }
