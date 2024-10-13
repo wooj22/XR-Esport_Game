c9db253990f75354e11c1beec94ca6492cd2dc61
woojeong <77192050+wooj22@users.noreply.github.com>
2024-10-13 21:20:55 +0900

    🔨 Feat | 메인광장 맵 이동 + 연출

---
+++ Assets/02. Scripts/Main/MainManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour
+{
+    [Header("Managers")]
+    [SerializeField] MainSoundManager _mainSoundManager;
+    [SerializeField] MainSceneManager _mainSceneManager;
+
+    [Header("FadeOutImage")]
+    [SerializeField] Image fade_front;
+    [SerializeField] Image fade_right;
+    [SerializeField] Image fade_left;
+    [SerializeField] Image fade_down;
+
+    public void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void SwitchMap(string sceneName)
+    {
+        StartCoroutine(FadeOutEndSwitchMap(sceneName));
+        Debug.Log("맵 전환 " + sceneName);
+    }
+
+    /// 페이드인
+    IEnumerator FadeIn()
+    {
+        _mainSoundManager.PlayBGM();
+        float fadeCount = 1;
+
+        while (fadeCount > 0.001f)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+
+            fade_front.color = new Color(0, 0, 0, fadeCount);
+            fade_right.color = new Color(0, 0, 0, fadeCount);
+            fade_left.color = new Color(0, 0, 0, fadeCount);
+            fade_down.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+
+    /// 페이드아웃 후 맵 전환
+    IEnumerator FadeOutEndSwitchMap(string sceneName)
+    {
+        _mainSoundManager.PlaySFX("SFX_Main_openMap");
+
+        float fadeCount = 0;
+        while (fadeCount < 1.0f)
+        {
+            fadeCount += 0.01f;
+            yield return new WaitForSeconds(0.01f);
+
+            fade_front.color = new Color(0, 0, 0, fadeCount);
+            fade_right.color = new Color(0, 0, 0, fadeCount);
+            fade_left.color = new Color(0, 0, 0, fadeCount);
+            fade_down.color = new Color(0, 0, 0, fadeCount);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        _mainSceneManager.OnLoadSceneByName(sceneName);
+    }
+}
--- Assets/02. Scripts/Main/MainPlayerController.cs
+++ Assets/02. Scripts/Main/MainPlayerController.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class MainPlayerController : MonoBehaviour
 {
-    MainSceneManager _mainSceneManager;
+    MainManager _mainManager;
 
     private void Start()
     {
-        _mainSceneManager = GameObject.Find("SceneManager").GetComponent<MainSceneManager>();
+        _mainManager = GameObject.Find("MainManager").GetComponent<MainManager>();
     }
 
-
     // 플레이어와 충돌한 콜라이더의 name씬으로 이동
+    // 충돌중일때 게이지를 채우고, 게이지가 다 차면 swithMap()을 호출하는 방식으로 변경해야함
     private void OnTriggerEnter(Collider other)
     {
         string sceneName = other.gameObject.name;
-        _mainSceneManager.OnLoadSceneByName(sceneName);
+        _mainManager.SwitchMap(sceneName);
     }
 }
