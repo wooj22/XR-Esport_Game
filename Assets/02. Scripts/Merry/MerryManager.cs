06e09c4cb1cbaade2c158f9cb10a5bdd19ec727b
woojeong <77192050+wooj22@users.noreply.github.com>
2024-10-28 00:55:05 +0900

    🔨 Feat | 회전목마 플로우, 메인 연결, 시작/종료 연출

--- Assets/02. Scripts/Merry/MerryManager.cs
+++ Assets/02. Scripts/Merry/MerryManager.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class MerryManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Map Data")]
+    [SerializeField] float runningtime;
+
+    [Header("Managers")]    
+    [SerializeField] MerryUIManager _merryUIManager;
+    [SerializeField] MerrySoundManager _merrySoundManager;
+    [SerializeField] MerrySceneManager _merrySceneManager;
+
+    private Coroutine merryop;
+    private Coroutine merrytermi;
+
+    private void Start()
     {
-        
+        MerryMapStartSetting();
+        merryop = StartCoroutine(MerryOeration());
+        merrytermi = StartCoroutine(MerryTermination());
     }
 
-    // Update is called once per frame
-    void Update()
+    /// 시작 셋팅
+    private void MerryMapStartSetting()
     {
-        
+        _merrySoundManager.PlayBGM();
+        _merryUIManager.FadeInImage();
+    }
+
+    /// 운행
+    IEnumerator MerryOeration()
+    {
+        yield return null;
+    }
+
+    /// 운행 종료
+    IEnumerator MerryTermination()
+    {
+        yield return new WaitForSeconds(runningtime);
+        _merrySoundManager.StopBGM();
+        _merryUIManager.FadeOutImage();
+        yield return new WaitForSeconds(5f);
+        _merrySceneManager.LoadMainMenuMap();
+    }
+
+    /// 중간 강제 이동
+    public void MerryStop()
+    {
+        StopCoroutine(merryop);
+        StopCoroutine(merrytermi);
+        StartCoroutine(MerryStopEnging());
+    }
+
+    IEnumerator MerryStopEnging()
+    {
+        _merrySoundManager.StopBGM();
+        _merryUIManager.FadeOutImage();
+        yield return new WaitForSeconds(5f);
+        _merrySceneManager.LoadMainMenuMap();
     }
 }
